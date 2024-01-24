6d1923d26991384d25d8689c407da26e4671e306
OUMAR3 <oumaralitori1@gmail.com>
2024-01-24 14:35:29 +0100

    add games component

--- src/index.js
+++ src/index.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React, {Component} from "react";
 
-class Square extends React.Component {
-    render() {
+
+class Square extends React.Component {  
+    render(){    
+            return (
+                <button
+                className="square"
+                onClick={() => this.props.onClick()} >
+                {this.props.value}     
+                </button>
+            );
+    }
+}
+
+class Board extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+      };
+    }
+  
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';    this.setState({squares: squares}); 
+     }
+    renderSquare(i) {
       return (
-        <button className="square">
-          {/* TODO */}
-        </button>
+        <Square
+          value={this.state.squares[i]}
+          onClick={() => this.handleClick(i)}
+        />
       );
     }
-  }
-  
-  class Board extends React.Component {
-    renderSquare(i) {
-      return <Square />;
-    }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Prochain joueur : X';
   
       return (
         <div>
@@ -42,25 +59,3 @@ class Square extends React.Component {
       );
     }
   }
-  
-  class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
-    }
-  }
-  
-  // ========================================
-  
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(<Game />);
-  
